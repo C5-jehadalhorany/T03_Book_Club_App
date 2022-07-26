a4a1250c20d8ac9d33d8bc3113c503ac227780ce
jehadalhorany <alhoranyjehad@gmail.com>
2022-07-26 22:54:14 +0300

    add function

---
+++ backend/controllers/rols/index.js
@@ -0,0 +1,27 @@
+const connection = require("../../models/db");
+
+const createRole = (req, res) => {
+  const { role } = req.body;
+  const query = "INSERT INTO roles (role ) VALUES (?);";
+  const data = [role];
+
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      return  res.json({
+        success: false,
+        message: "Server Error",
+        err: err,
+      });
+    }
+      res.json({
+      success: true,
+      massage: "Success role created",
+      result: result,
+    });
+  });
+};
+
+
+
+
+module.exports = {createRole};
