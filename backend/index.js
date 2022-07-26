a1dc7ba4e6021e730e241f3cbe4e2cd614cf01f7
jehadalhorany <alhoranyjehad@gmail.com>
2022-07-26 22:52:43 +0300

    add the router

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
-
 require("./models/db");
-
+//all require
 const booksRouter = require("./routes/books");
+const commentRouter = require("./routes/comments");
+const UsersRouter = require("./routes/users");
+const loginRouter = require("./routes/login");
+const registerRouter = require("./routes/register");
+const roleRouter = require("./routes/rols");
+const permissionRouter = require("./routes/permission");
 
 const app = express();
 const PORT = process.env.PORT || 5000
@@ -12,7 +17,19 @@ app.use(cors());
 app.use(express.json());
 
 //books
-app.use("/book", booksRouter)
+app.use("/book", booksRouter);
+//comment
+app.use("/comment", commentRouter);
+//admin/users
+app.use("/admin/users", UsersRouter);
+//login
+app.use("/login", loginRouter);
+//register
+app.use("/register", registerRouter);
+//rols
+app.use("/role", roleRouter);
+// permission 
+app.use("/permission", permissionRouter)
 
 
 
