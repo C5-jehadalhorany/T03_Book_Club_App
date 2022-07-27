745fa94a42af99fdb8f92704f6f389b9ab28e00b
jehadalhorany <alhoranyjehad@gmail.com>
2022-07-28 01:09:58 +0300

    Edit in books

---
+++ frontend/src/components/Books/index.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom";
+import { login, logout } from "../../redux/reducers/user";
+
+
+
+const BooksFunction = () => {
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const { token, Islogin, role_id, username } = useSelector((state) => {
+        return {
+            token: state.auth.token,
+            Islogin: state.auth.Islogin,
+            role_id: state.auth.role_id,
+            username: state.auth.username
+        };
+    });
+
+
+    const booksRender = () => {
+        axios.get(`http://localhost:5000/book`,{token:token}).then((result) => {
+            console.log(result.data);
+
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+
+
+    useEffect(() => {
+        booksRender()
+    }, [])
+
+    return <button onClick={()=>{booksRender()}}>as</button>
+}
+
+export default BooksFunction
